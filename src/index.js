1b37369c5a0a1bf829db4d606cb48d76c5812a5d
Alejuda <ignacio.fino@mi.unc.edu.ar>
2023-02-07 21:34:16 -0300

    added all the forms and styled the index for navigating throw all the forms

--- src/index.js
+++ src/index.js
@@ -2,12 +2,28 @@ const express = require("express");
 
 const app = express();
 
+const path = require("path");
+
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
 });
 
+app.use(express.static(__dirname + "/public"));
+
 app.get("/", (req, res) => {
-    res.send('Hola! Bienvenid@!')
+  res.sendFile(path.join(__dirname, "views/index.html"));
 });
+
+app.get("/contacto", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/contacto.html"));
+})
+
+app.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/login.html"));
+})
+
+app.get("/registro", (req, res) => {
+  res.sendFile(path.join(__dirname, "views/registro.html"));
+})
